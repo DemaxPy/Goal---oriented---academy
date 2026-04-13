9b09d6f0aae7ac4aecffcb79567884c48a46d1f9
DemaxPy <demaxplayz@gmail.com>
2026-04-13 17:29:59 +0400

    commit

---
+++ level 20/homework.py
@@ -0,0 +1,36 @@
+# 1) while  loop ი იმეორებს  იმ რაღაცას სანამ პირობა ჭეშმარიტია.  ხოლო for loop ი იმეორებს იქამდე სადამდეც range ში რიცხვი წერია
+
+# 2)
+
+password = "goa best"   
+
+user_input = input("enter your password: ")
+
+while user_input != password:
+    print(" password incorrect! try again")
+    user_input = input( "enetr your password:" )
+
+print(" password correct. welcome!")
+
+
+# 3)
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
+
+
+# 4)
+
+i = 0
+
+while i <= 20:
+    print(i)
+    i += 2
+
+ # 5)
+
+ #while და for loop - ები გამოიყენება რომ მრავალჯერ კოდი ავტომატყრად შესრულდეს
+ 
