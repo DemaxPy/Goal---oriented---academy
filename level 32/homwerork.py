ddd81d85a067bc456fcb0a42991c80eb22a842cd
DemaxPy <demaxplayz@gmail.com>
2026-05-25 15:44:28 +0400

    first commit

---
+++ level 32/homwerork.py
@@ -0,0 +1,84 @@
+
+#1)
+
+
+grades = [91, 85, 60, 77, 98]
+
+# First grade
+print(grades[0])
+
+# Third grade
+print(grades[2])
+
+# Last grade
+print(grades[-1])
+
+# Change the fourth grade
+grades[3] = 82
+
+# Print the whole list
+print(grades)
+
+
+
+
+
+
+
+#2)
+
+
+steps = [4500, 8000, 10200, 3100, 9000]
+
+# Second day
+print(steps[1])
+
+# Fourth day
+print(steps[3])
+
+# Last day
+print(steps[-1])
+
+# Update the first day's steps
+steps[0] = 5200
+
+# Print the whole list
+print(steps)
+
+
+
+#3)
+
+prices = [15, 42, 9, 120, 35, 64]
+
+# First product
+print(prices[0])
+
+# Fourth product
+print(prices[3])
+
+# Last product
+print(prices[-1])
+
+# Update the third product price
+prices[2] = 7
+
+# Print the whole list
+print(prices)
+
+
+
+
+#4)
+#  List — არის მონაცემების სია, სადაც ერთ ცვლადში რამდენიმე მნიშვნელობის შენახვა შეგვიძლია.
+#  index არის ელემენტის პოზიციის ნომერი list-ში. ინდექსები იწყება 0-დან.
+
+
+
+
+
+
+
+
+
+
