9193f9e7a4b5a4d86f5c1f708ec4c1a89eae0ab3
DemaxPy <demaxplayz@gmail.com>
2026-05-30 16:59:09 +0400

    first commit

--- level 32/classwork.py
+++ level 32/classwork.py
@@ -4,3 +4,26 @@ if number >= 0:
     print("The number is positive")
 else:
     print("The number is negative")
+
+
+
+
+
+
+
+    sales = [45, 50, 62, 40, 55, 90, 105]
+
+# first element
+print(sales[0])
+
+# second element
+print(sales[2])
+
+# last element
+print(sales[-1])
+
+# change meaning
+sales[3] = 48
+
+# print remaked list
+print(sales)
