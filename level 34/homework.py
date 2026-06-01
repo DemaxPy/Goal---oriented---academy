83f3a248012d98b375587ebb8268a25eaaa2a07a
DemaxPy <demaxplayz@gmail.com>
2026-06-01 15:11:41 +0400

    first commit

---
+++ level 34/homework.py
@@ -0,0 +1,16 @@
+sales = [45, 50, 62, 40, 55, 90, 105]
+
+# First element (Monday sales)
+print(sales[0])
+
+# Third element (Wednesday sales)
+print(sales[2])
+
+# Last element using a negative index
+print(sales[-1])
+
+# Change Thursday sales from 40 to 48
+sales[3] = 48
+
+# Print the updated list
+print(sales)
