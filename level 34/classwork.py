07ef9220f926502734c91d4f531fcc17f6842169
DemaxPy <demaxplayz@gmail.com>
2026-06-05 16:44:34 +0400

    first commit

--- level 34/classwork.py
+++ level 34/classwork.py
@@ -1,4 +1,4 @@
-#List არის მონაცემების სია, სადაც რამდენიმე მნიშვნელობას ერთად ვინახავთ. სიაში თითოეულ ელემენტს აქვს თავისი Index (ნომერი), 
+#List არის მონაცემების სია, სადაც რამდენიმე მნიშვნელობას ერთად ვინახავთ. სიაში თითოეულ ელემენტს აქვს თავისი Index , 
 # რომელიც Python-ში 0-დან იწყება. Indexing კი ნიშნავს სიიდან ელემენტის ამოღებას მისი ინდექსის გამოყენებით.
 
 
@@ -7,3 +7,19 @@ fruits = ["apple", "banana", "orange"]
 print(fruits[0])  # apple
 print(fruits[1])  # banana
 print(fruits[2])  # orange
+
+
+
+fruits = ["Apple", "Banana", "Peach", "Watermelon", "Melon", "Kiwi", "Strawberry"]
+
+# First element
+print(fruits[0])
+
+# Last element (using a negative index)
+print(fruits[-1])
+
+# Slice the middle part
+print(fruits[2:5])
+
+# Print every second fruit
+print(fruits[::2])
