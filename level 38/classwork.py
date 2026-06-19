f97b210072e32475fbbe57488875d8b5fefe585e
DemaxPy <demaxplayz@gmail.com>
2026-06-19 17:08:47 +0400

    first commit

---
+++ level 38/classwork.py
@@ -0,0 +1,14 @@
+# 1) python ში  არის ჩაშენებული ფუნქციები, რომლებიცაა: print(), def, greet, name= , while , for , range და ა.შ
+
+# 2) 
+ 
+
+def greet():
+    print("Hello")
+
+for i in range(10):
+    greet()
+
+
+
+
